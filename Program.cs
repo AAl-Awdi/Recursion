d25980055215143fee67c98f1ec699d89b64970f
AAl-Awdi <127621473+AAl-Awdi@users.noreply.github.com>
2023-10-26 02:20:06 +0100

    Update Program.cs
    1. Using recursion to print a given variable decreasing by one, until reaching zero, and all unfolding at the end.
    
    2. Using recursion to print a variable increasing by one until it reaches a target.
    
    3. Using recursion to add a variable decreasing by one, until reaching zero, adding each iteration to a string variable, and then printing said string.
    
    4. Using recursion to multiply two integer variables together, with "+" and "-" only.
    
    5. Using recursion to increase an integer variable by a *positive* integer power, with "*" only.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,15 +14,29 @@ namespace recursion
             printNumbers(5);
             Console.ReadLine();
 
-            range(5,10);
+            range(5, 10);
             Console.ReadLine();
 
             getNumberString(5);
             Console.WriteLine(numberList);
             Console.ReadLine();
+
+            Console.WriteLine("5 x 10 = {0}", multiply(5, 10));
+            Console.ReadLine();
+            Console.WriteLine(" 7 x -7 = {0}", multiply(7, -7));
+            Console.ReadLine();
+            Console.WriteLine(" -4 x -8 = {0}", multiply(-4, -8));
+            Console.ReadLine();
+
+            Console.WriteLine("2 ^ 5 = {0}", power(2, 5));
+            Console.ReadLine();
+            Console.WriteLine(" -3 ^ 3 = {0}", power(-3, 3));
+            Console.ReadLine();
+            Console.WriteLine(" -3 ^ 4 = {0}", power(-3, 4));
+            Console.ReadLine();
         }
-        
-        
+
+
         static void printNumbers(int n)
         {
             if (n > 0)
@@ -36,7 +50,7 @@ namespace recursion
         static void range(int start, int stop)
         /* Displays numbers between start and stop - start <= stop */
         {
-            
+
             if (start == stop)
             {
                 Console.WriteLine("Thenumber has reached its target");
@@ -44,10 +58,9 @@ namespace recursion
             else
             {
                 Console.WriteLine("number = {0}", start);
-                range(start+1, stop);
+                range(start + 1, stop);
             }
         }
-
         static string getNumberString(int n)
         /* Displays numbers between start and stop - start <= stop */
         {
@@ -64,17 +77,39 @@ namespace recursion
             return "Fail";
         }
 
+        static int inc(int x)
+        {
+            return x + 1;
+        }
+
+        static int dec(int x)
+        {
+            return x - 1;
+        }
+
         static int multiply(int x, int y)
         {
-            if (y == 1)
-                return   ??  ;
+            if (y == 0) { return 0; }
+            else if (y == 1) { return x; }
+            else if (y == -1) { return -x; }
             else
+            {
+                if (y < 0) { return -x + multiply(x, inc(y)); }
+                if (y > 0) { return  x + multiply(x, dec(y)); }
+            }
+            return 999; // error case
+        }
 
-
-    }
-
-
-
-
+        static int power(int x, int y)
+        { // Negative y wont work, as they produce decimals, so we ignore negative y.
+            if (y == 0) { return 0; }
+            else if (y == 1) { return x; }
+            else
+            {
+                if (x < 0) { return (-x) * power(x, y - 1); }
+                if (x > 0) { return x * power(x, y - 1); }
+            }
+            return 999; // error case
+        }
     }
 }
